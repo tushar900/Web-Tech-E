e3af24f86571aab7f3f637c3ac4d9a7d64b3a0e2
Nazmul Islam <72748188+tushar900@users.noreply.github.com>
2021-01-04 18:39:57 +0600

    Add files via upload

---
+++ task 6 heder.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="homepage.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="userdetiles.php">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="addproduct.php">Add Product</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="action.php?logout=logout">Logout</a>
+    </li>
+  </ul>
+</nav>
